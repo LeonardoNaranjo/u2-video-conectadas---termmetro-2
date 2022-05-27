4db2756bdfb2220b8c9941f7e9d93ca0452e8f57
LeonardoNaranjo <47719586+LeonardoNaranjo@users.noreply.github.com>
2022-05-27 14:35:48 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    while (input.temperature() > 25) {
+        basic.showNumber(input.temperature())
+        basic.showIcon(IconNames.Happy)
+    }
+})
